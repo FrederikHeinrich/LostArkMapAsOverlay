d8fc6104e262f39e0d1984fc44907cd7b970326d
Freddi <mail@FrederikHeinrich.de>
2022-03-02 10:34:19 +0100

    app updater

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const {
   app,
-  BrowserWindow
+  BrowserWindow,
+  dialog
 } = require('electron');
 const path = require('path');
 const fs = require('fs');
+const {
+  autoUpdater
+} = require("electron-updater")
+
+const isDev = require("electron-is-dev")
 
 app.disableHardwareAcceleration();
 
@@ -15,7 +21,6 @@ if (require('electron-squirrel-startup')) {
 
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 600,
     height: 600,
@@ -28,8 +33,6 @@ const createWindow = () => {
 
   mainWindow.setAlwaysOnTop(true, 'screen');
 
-  // and load the index.html of the app.
-  //mainWindow.loadFile(path.join(__dirname, 'index.html'));
   mainWindow.loadURL("https://lostarkmap.com/");
   mainWindow.on("ready-to-show", () => {
     mainWindow.show();
@@ -42,19 +45,15 @@ const createWindow = () => {
       .then(console.log('JavaScript Executed Successfully'));
 
   });
-
+  if (!isDev) {
+    autoUpdater.checkForUpdates();
+  }
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -62,12 +61,20 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+autoUpdater.on("update-available", (_event, releseNotes, releseName) => {
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Okay"],
+    title: "Update!",
+    message: process.platform == "win32" ? releseNotes : releseName,
+    detail: "A new version is being downloades"
+  }
+  dialog.showMessageBox(dialogOpts, (response) = {
+
+  })
+});
